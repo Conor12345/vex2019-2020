8b1c84c78fa75178d924df0f0f917dbbe8e91dc4
Conor <me@conorn.xyz>
2020-02-10 17:12:27 +0000

    update code after regional

--- .vscode/cquery_cached_index/c@@users@conor@google drive@newvex@vex2/src@opcontrol.cpp
+++ .vscode/cquery_cached_index/c@@users@conor@google drive@newvex@vex2/src@opcontrol.cpp
@@ -1,4 +1,7 @@
 #include "main.h"
+#include "sys/time.h"
+#include <string>
+#include "chrono"
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -15,7 +18,8 @@
  */
 
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
+	pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
 	// -=+=- Motor setup -=+=- //
 	pros::Motor left_front_mtr(LEFTFRONTMTR); // forward
 	pros::Motor right_front_mtr(RIGHTFRONTMTR); // reverse
@@ -30,6 +34,7 @@ void opcontrol() {
 	right_intake_mtr.set_reversed(true);
 
 	pros::Motor ramp_mtr(RAMPMTR);
+	ramp_mtr.set_brake_mode(MOTOR_BRAKE_HOLD);
 
 	pros::ADIPotentiometer autonPot(1);
 
@@ -37,19 +42,21 @@ void opcontrol() {
 	int joysticks [4] = {0, 0, 0, 0};
 	float multiplier = 1.0;
 
+	std::string auton = "";
+
 	while (true) {
 		// -=+=- Drive Control -=+=- //
-		if (master.get_digital(DIGITAL_X)) {
+		if (controller.get_digital(DIGITAL_X)) {
 			multiplier = 0.4;
 		}
 		else {
 			multiplier = 1.0;
 		}
 
-		joysticks[0] = master.get_analog(ANALOG_RIGHT_X); // joysticks[0]
-		joysticks[1] = master.get_analog(ANALOG_RIGHT_Y); // joysticks[1]
-		joysticks[2] = master.get_analog(ANALOG_LEFT_Y); // joysticks[2]
-		joysticks[3] = master.get_analog(ANALOG_LEFT_X); // joysticks[3]
+		joysticks[0] = controller.get_analog(ANALOG_RIGHT_X); // joysticks[0]
+		joysticks[1] = controller.get_analog(ANALOG_RIGHT_Y); // joysticks[1]
+		joysticks[2] = controller.get_analog(ANALOG_LEFT_Y); // joysticks[2]
+		joysticks[3] = controller.get_analog(ANALOG_LEFT_X); // joysticks[3]
 
 		right_front_mtr.move( (joysticks[2] - joysticks[3]) * multiplier );
 		left_front_mtr.move( (joysticks[2] + joysticks[3]) * multiplier );
@@ -59,11 +66,11 @@ void opcontrol() {
 		right_lift_mtr.move(joysticks[1]);
 
 		// -=+=- Intake Control -=+=- //
-		if (master.get_digital(DIGITAL_R2)) {
+		if (controller.get_digital(DIGITAL_R2)) {
 			left_intake_mtr.move(75);
 			right_intake_mtr.move(75);
 		}
-		else if (master.get_digital(DIGITAL_R1)){
+		else if (controller.get_digital(DIGITAL_R1)){
 			left_intake_mtr.move(-127);
 			right_intake_mtr.move(-127);
 		}
@@ -73,10 +80,10 @@ void opcontrol() {
 		}
 
 		// -=+=- Intake Control -=+=- //
-		if (master.get_digital(DIGITAL_L1)) {
+		if (controller.get_digital(DIGITAL_L1)) {
 			ramp_mtr.move(50);
 		}
-		else if (master.get_digital(DIGITAL_L2)){
+		else if (controller.get_digital(DIGITAL_L2)){
 			ramp_mtr.move(-50);
 		}
 		else {
@@ -87,26 +94,27 @@ void opcontrol() {
 		int potValue = autonPot.get_value() / 40.96;
 		pros::lcd::print(2, "Pot: %d", potValue);
 		if (potValue < 20) {
-			pros::lcd::print(1, "Auton: Red Front");
+			auton =  "Auton: Red Front";
 		}
 		else if (potValue < 40) {
-			pros::lcd::print(1, "Auton: Red Back");
+			auton =  "Auton: Red Back";
 		}
 		else if (potValue < 60) {
-			pros::lcd::print(1, "Auton: Blue Front");
+			auton =  "Auton: Blue Front";
 		}
 		else if (potValue < 80) {
-			pros::lcd::print(1, "Auton: Blue Back");
+			auton =  "Auton: Blue Back";
 		}
 		else if (potValue < 95) {
-			pros::lcd::print(1, "Auton: Testing");
+			auton =  "Auton: Testing";
 		}
 		else {
-			pros::lcd::print(1, "None");
+			auton =  "None";
 		}
 
-		pros::lcd::print(2, "Motor temp 1 %d", left_lift_mtr.get_temperature());
-		pros::lcd::print(3, "Motor temp 2 %d", right_lift_mtr.get_temperature());
+		pros::lcd::print(1, "Auton: %s %d", auton, potValue);
+		pros::lcd::print(2, "Motor temp 1 %f", left_lift_mtr.get_temperature());
+		pros::lcd::print(3, "Motor temp 2 %f", right_lift_mtr.get_temperature());
 
 		pros::delay(20);
 	}
